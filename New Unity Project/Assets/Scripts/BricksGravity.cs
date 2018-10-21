ee383677cc23e5ad79bcb936a3756950a3fa6829
Alexandre <alexandre.nguyen@infotel.com>
2018-10-21 20:51:12 +0200

    :sparkles: gravity added !
    press up and down

---
+++ New Unity Project/Assets/Scripts/BricksGravity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BricksGravity : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetKey("up")) {
+			gameObject.GetComponent<Rigidbody>().useGravity = false;
+		} else if (Input.GetKey("down")) {
+			gameObject.GetComponent<Rigidbody>().useGravity = true;
+		}
+	}
+}
